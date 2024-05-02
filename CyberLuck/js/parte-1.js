4a66095cf00f527fc08607bfe5d2643fb4fcf0ab
Davi Nascimento <davileite2007@gmail.com>
2024-05-01 22:51:37 -0300

    Primeira adição de arquivos

---
+++ CyberLuck/js/parte-1.js
@@ -0,0 +1,134 @@
+var nameSlct;
+var levelSlct;
+var nome;
+var level;
+var limite;
+
+setTimeout(() => {
+    $('.screen').append(
+        '<div class="introducao">\
+            <div class="nome">\
+                <p>IDENTIFIQUE-SE:</p>\
+                <div class="name-wraper">\
+                    <input id="name" type="text">\
+                </div><!--name-wraper-->\
+            </div><!--nome-->\
+        </div><!--introducao-->'
+    );
+    $('#name').trigger('focus');
+    
+    $('body').click(function(){
+        if(nameSlct != true){
+            $('#name').trigger('focus');
+        }
+    })
+    
+    nameSelect();
+
+}, 1000);
+
+function nameSelect(){
+    $('#name').keyup(function(e) {
+        // Verifica se a tecla pressionada é a tecla Enter (código 13)
+        if (e.which == 13) {
+
+            nome = $('#name').val();
+    
+            if(nome == ''){
+                return false;
+            }
+            
+            let amount = nome.length
+
+            if(amount > 1 && amount < 20){
+                if(nome.match(/^[A-Za-z0-9]+$/)){
+                    $('.name-wraper').html('<p style="margin-top: 5px; color: white">' + nome  + '</p>');
+
+                    nameSlct = true;
+
+                    levelSelect();
+
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
+    });
+}
+
+function levelSelect(){
+    setTimeout(() => {
+        $('.introducao').append(
+            '<div class="nivel">\
+            <p>SELECIONE NÍVEL DE ACESSO AO SISTEMA:</p>\
+            <div class="niveis">\
+                <span id="1">\
+                    [1]\
+                </span>\
+                <span id="2">\
+                    [2]\
+                </span>\
+                <span id="3">\
+                    [3]\
+                </span>\
+            </div><!--niveis-->\
+        </div><!--nivel-->'
+        )
+        $('.niveis span').click(function(){
+            if(levelSlct != true){
+                levelSlct = true;
+                $(this).css('color', 'white');
+                $('.niveis span').css('cursor', 'auto');
+                level = Number($(this).attr('id'));
+                limite = definirLimite(level);
+
+                $('.screen').append('<div class="game-screen"></div>')
+
+                inicializar();
+            }
+        })
+    }, 500);
+};
+
+function inserir(destino, tag, conteudo, tempo, style=false, br=false){
+    setTimeout(() => {
+        if(br == false){
+            let first_tag;
+            if(style == false){
+                first_tag = '<' + tag + '>'
+            }else{
+                first_tag = '<' + tag + ' style="' + style + '">'
+            }
+            destino.append(first_tag +conteudo+'</'+tag+'>')
+        }else{
+            destino.append('<' + tag + '>' + conteudo)
+        }
+        
+    }, tempo);
+}
+
+function inicializar(){
+    let el = $('.game-screen');
+    inserir(el, 'p', 'Boas-vindas, ' + nome + '.', 500);
+    inserir(el, 'p', 'Executando algoritmo de verificação de segurança...', 1000);
+    inserir(el, 'p', 'Carregando módulos do firewall...', 1500);
+    inserir(el, 'p', 'import time', 1800);
+    inserir(el, 'p', 'def encrypt_message(message):', 2100);
+    inserir(el, 'p', 'encrypted_message = ""', 2400);
+    inserir(el, 'p', 'for char in message:', 2700);
+    inserir(el, 'p', 'encrypted_char = str(ord(char))', 3000);
+    inserir(el, 'p', 'encrypted_message += encrypted_char + " "', 3300);
+    inserir(el, 'p', 'time.sleep(0.1)', 3600);
+    inserir(el, 'p', 'return encrypted_message', 3900);
+    inserir(el, 'p', 'message = "The bear is in the circus"', 4200);
+    inserir(el, 'p', 'Encrypting message...', 4500);
+    inserir(el, 'p', 'Encrypted message: 84 104 101 32 98 101 97 114 32 105 115 32 105 110 32 116 104 101 32 99 105 114 99 117 115', 7000);
+    inserir(el, 'p', 'In´´cializ#ndo~ //0P_SY5TEM//, agu%rd&.', 8000);
+    pularLinha(el, 8999);
+    inserir(el, 'p', '// PERIGO // PERIGO // PERIGO // PERIGO // PERIGO // PERIGO // PERIGO // PERIGO // PERIGO // PERIGO //', 9000);
+    setTimeout(() => {
+        entrada_hacker();
+    }, 10000);
+}
---
+++ CyberLuck/js/parte-2.js
@@ -0,0 +1,124 @@
+var tentativa = 0;
+
+function numeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function progressoVirus(tempo){
+    setTimeout(() => {
+        $('.game-screen').append('<div class="virus-bar-wraper"></div>');
+        $('.virus-bar-wraper').append('<p>Progresso do vírus no sistema:</p>');
+        $('.virus-bar-wraper').append('<div class="virus-bar"></div>');
+        $('.virus-bar').append('<span class="porcentagem"></span>');
+        $('.virus-bar').append('<div class="bar-space"></div>');
+        $('.bar-space').append('<div class="loaded-bar" style="float: left"></div>');
+        $('.virus-bar').append('<div style="clear: both"></div>');
+
+        let min;
+        let max;
+        let num;
+
+        if(tentativa == 0){
+            min = 2
+            max = 3;
+
+        }else if(tentativa == 1){
+            min = 10;
+            max = 21;
+
+        }else if(tentativa == 2){
+            min = 22;
+            max = 41;
+            
+        }else if(tentativa == 3){
+            min = 42;
+            max = 61;
+            
+        }else if(tentativa == 4){
+            min = 62;
+            max = 81;
+            
+        }else if(tentativa == 5){
+            min = 82;
+            max = 99;
+            
+        }else{
+            min = 100;
+            max = 100;
+        }
+
+        num = numeroAleatorio(min,max);
+        $('.loaded-bar').css('width', num + '%');
+        $('.porcentagem').html(num + '%');
+        $('.bar-space').css('width', 'calc(100% - ' + ($('.porcentagem').width() + 10) + 'px)')
+        console.log(tentativa, min, max, num);
+        
+    }, tempo);
+}
+
+function transformadorNum(num){
+    if(num < 10){
+        num = '0' + num
+    }
+    return num
+};
+
+function DataHora(tempo){
+    setTimeout(() => {
+        let dataAtual = new Date();
+        let ano = transformadorNum(dataAtual.getFullYear());
+        let mes = transformadorNum(dataAtual.getMonth() + 1);
+        let dia = transformadorNum(dataAtual.getDate());
+        let horas = transformadorNum(dataAtual.getHours());
+        let minutos = transformadorNum(dataAtual.getMinutes());
+        let segundos = transformadorNum(dataAtual.getSeconds());
+
+        $('.game-screen').append('<div class="tempo"></div>');
+        $('.tempo').append('<p>' + dia + '-' + mes + '-' + ano + '</p>\
+        <p>' + horas + ':' + minutos + ':' + segundos + '</p>');
+        
+    }, tempo);
+}
+
+function definirLimite(level){
+    let limite;
+    if(level == 1){
+        limite = '<span style="color: white">1 até 8</span>'
+    }else if(level == 2){
+        limite = '<span style="color: white">1 até 16</span>'
+    }else{
+        limite = '<span style="color: white">1 até 24</span>'
+    }
+    return limite;
+}
+
+function pularLinha(destino, tempo, duas=false){
+    if(duas == false){
+        inserir(destino, 'br', '', tempo, false, true);
+    }else{
+        inserir(destino, 'br', '', tempo, false, true);
+        inserir(destino, 'br', '', tempo, false, true);
+    }   
+}
+
+function chatCyber(destino, tempo){
+    inserir(destino, 'span', 'cYber_death0', tempo, 'color: white');
+}
+
+function entrada_hacker(){
+    let el = $('.game-screen');
+    pularLinha(el, 500)
+    inserir(el, 'p', '//AVISO DO LOG// cYber_death0 conectou ao sistema há 5 minutos.', 501);
+    pularLinha(el, 1500);
+    chatCyber(el, 1501);
+    inserir(el, 'span', ': MWAHAHAHAHAHAHA! Seu PC foi infectado pelo meu vírus! Ele está sendo carregado no seu sistema neste momento e não há nada que você possa fazer! A não ser que... Impossível. Você não vai adivinhar o código secreto a tempo, ' + nome +  ' hahaha!', 1502);
+    pularLinha(el, 2500, true);
+    inserir(el, 'p', '//FIREWALL DETECTOU CARREGAMENTO DE VÍRUS LETAL AO SISTEMA//', 2501)
+    inserir(el, 'p', '//TENTATIVA DE NEUTRALIZAR VÍRUS EM ANDAMENTO//', 3000);
+    inserir(el, 'p', 'AVISO: FIREWALL SOBRECARREGADO. ENTRADA MANUAL NECESSÁRIA DE [USER ' + nome + '].', 3500);
+    inserir(el, 'p', 'DETECTADO CÓDIGO DE NÚMERO ENTRE ' + limite + '.', 4000);
+    pularLinha(el, 4500);
+    progressoVirus(4501);
+    pularLinha(el, 5000);
+    DataHora(5500);
+}
